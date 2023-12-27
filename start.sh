7d09858f348fb05e45b1046167a3d51c0fd84e18
ggzh0195 <56225540+ggzh0195@users.noreply.github.com>
2023-12-27 19:19:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,34 +1,57 @@
+
 #!/bin/bash
 
-if ! command -v npm &> /dev/null
-then
-    read -p "npm is not installed. Do you want to install nodejs and npm? (y/n)" choice
-    case "$choice" in
-      y|Y )
-        echo "Installing nvm..."
-        export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-        source ~/.bashrc
-        nvm install --lts
-        nvm use --lts;;
-      n|N )
-        echo "Nodejs and npm will not be installed."
-        exit;;
-      * )
-        echo "Invalid option. Nodejs and npm will not be installed."
-        exit;;
-    esac
+# 脚本开始时的声明
+echo "                                              
+安卓自动部署酒馆
+作者：慢性鼻炎
+版本：Ver2.3.7
+来自：Claude2.0先行破限组
+群号：704819371             
+Discord群组链接：https://discord.gg/HWNkueX34q 
+
+注意：
+1、此程序是完全免费的！严禁任何形式的倒卖。
+2、除了本群外，其他地方的转发需得到授权。
+3、需要一个稳定的霍格沃兹（魔法）网络环境。
+4、全程都要挂梯子，不仅仅是安装，玩的时候也要！
+5、关于cookie的获取，请参考其他相关教程。
+"
+
+# 如果SillyTavern和Clewd都未安装，则更新pkg和npm
+if [[ ! -d "SillyTavern" ]]; then
+    echo "正在更新pkg..."
+    yes | pkg update
+    yes | pkg upgrade
 fi
 
-# if running on replit patch whitelist
-if [ ! -z "$REPL_ID" ]; then
-  echo -e "Running on Repl.it... \nPatching Whitelist..."
-  sed -i 's|whitelistMode = true|whitelistMode = false|g' "config.conf"
+# 检查并安装必要的软件包
+if ! command -v git &> /dev/null; then
+	echo "正在安装git..."
+    yes | pkg install git
 fi
 
-echo "Installing Node Modules..."
-npm i --no-audit
+if ! command -v node &> /dev/null; then
+	echo "正在安装nodejs..."
+    yes | pkg install nodejs
+fi
 
-echo "Entering SillyTavern..."
-node "$(dirname "$0")/server.js"
+echo "准备开始检测文件完整性"
+echo "注：文件完整性只能检测部分关键文件是否存在"
+
+# 循环检查并拉取SillyTavern
+while true; do
+    echo "正在检查SillyTavern文件完整性..."
+    if [ ! -f "SillyTavern/start.sh" ]; then
+        echo "SillyTavern文件不完整，将进行拉取。"
+        rm -rf SillyTavern
+        git clone https://github.com/SillyTavern/SillyTavern
+    else
+        break
+    fi
+done
+
+# 后台启动SillyTavern
+cd 
+cd SillyTavern
+bash start.sh &
